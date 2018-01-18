84e858a0b4f633ee714abb1ff7b1415d520c7946
mkassjanski <ankoupl@o2.pl>
2018-01-18 08:57:07 +0100

    poprawki stylu

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount ActionCable.server => '/cable'
   devise_for :users
-  #get 'rooms/:picture_id' => 'draw#show'
-  resources :rooms, only: [:new, :create, :show, :index]
+  resources :rooms
   root 'welcome#index'
 end
